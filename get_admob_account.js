972f1fa0de240c9c1b638725a750f8a3cb1aae34
santaux <santaux@gmail.com>
2015-07-23 00:38:33 +0300

    Added one more timeout for admob account step

--- get_admob_account.js
+++ get_admob_account.js
@@ -8,7 +8,9 @@ setTimeout(function() {
     chrome.storage.local.set({"current_account_id": admob_account_id[0]});
 
     console.log('done! redirecting back...');
-    document.location.href = "https://console.developers.google.com/project";
+    setTimeout(function() {
+      document.location.href = "https://console.developers.google.com/project";
+    }, 2000);
   } else {
     alert('An error occured on your admob account id copying. If you are not logged in, please try again after authorization');
     chrome.storage.local.remove("reporting_tab_id");
