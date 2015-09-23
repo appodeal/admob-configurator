013b6943a0fdb397588b4869fb975f97414f5f33
Roman Ralovets <roman@ralovets.com>
2015-09-23 17:19:06 +0300

    new project create

--- reporting.js
+++ reporting.js
@@ -4,10 +4,27 @@ var is_working = false;
 var project_name_interval = null;
 var project_created_interval = null;
 
+function run_script(code) {
+  var script = document.createElement('script');
+  script.appendChild(document.createTextNode(code));
+  document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+function loadJquery(complete) {
+  var jq = document.createElement('script');
+  jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
+  document.getElementsByTagName('head')[0].appendChild(jq);
+
+  setTimeout(function() {
+    console.log("Jquery loaded");
+    complete();
+  }, 3000)
+}
+
 function find_and_go_to_project() {
   var project_link = jQuery('a:contains("Appodeal")');
   if (project_link.length > 0) {
-    console.log('project found. redirecting...');
+    console.log('Project found. Redirecting.');
 
     var project_id = project_link.attr('href').match(/project\/(.+)$/)[1];
     document.location.href = 'https://console.developers.google.com/project/' + project_id + '/apiui/apiview/adsense/overview';
@@ -17,7 +34,7 @@ function find_and_go_to_project() {
 function find_and_go_to_created_project() {
   var project_link = jQuery('a:contains("Appodeal")');
   if (project_link.length > 0) {
-    console.log('project found. redirecting...');
+    console.log('Project found. Redirecting.');
 
     clearInterval(project_created_interval);
     var project_name = location.href.toString().match(/project\/([^\/]+)\/?$/)[1];
@@ -26,33 +43,23 @@ function find_and_go_to_created_project() {
 }
 
 function wait_for_project_name() {
-  console.log('waiting...');
+  console.log('Waiting for the project name.');
 
   if ($('[ng-model="project.name"]').length && !is_working) {
     is_working = true;
     clearInterval(project_name_interval);
-    //jQuery('[ng-model="project.name"]').val('Appodeal');
 
-    var jq = document.createElement('script');
-    jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
-    document.getElementsByTagName('head')[0].appendChild(jq);
+    loadJquery(function() {
+      console.log('Сhange the project name.');
 
-    console.log('added jquery to document body...');
+      var code = "jQuery('[ng-model=\"project.name\"]').val('Appodeal'); angular.element(jQuery('[ng-model=\"project.name\"]')).triggerHandler('input');";
+      run_script(code);
 
-    // give time for script to load, then type:
-    window.setTimeout(function() {
-      jQuery.noConflict();
+      // give time for script tag to be processed:
+      setTimeout(function() {
+        console.log('Click OK button.');
 
-      var script = document.createElement('script');
-      var code = "console.log('changing project name!'); jQuery('[ng-model=\"project.name\"]').val('Appodeal'); angular.element(jQuery('[ng-model=\"project.name\"]')).triggerHandler('input');";
-      script.appendChild(document.createTextNode(code));
-      document.getElementsByTagName('head')[0].appendChild(script);
-
-      // give time for scipt tag to be processed:
-      window.setTimeout(function() {
-        console.log('clicking the OK button...');
-
-        window.setTimeout(function() {
+        setTimeout(function() {
           if ($('input[name="tos"]')) {
             $('input[name="tos"]').click();
           }
@@ -62,18 +69,18 @@ function wait_for_project_name() {
           }
         }, 1000);
 
-        console.log('done! project name changed!');
-        console.log('waiting for created project name...');
+        console.log('Done. Project name changed! Waiting for created project name.');
         project_created_interval = setInterval( find_and_go_to_created_project, 2000 );
       }, 2000);
-    }, 2000);
+    })
   }
 }
 
 setTimeout(function() {
   find_and_go_to_project();
 
-  $('[ng-click="psCtrl.showCreateProjectDialog()"]').click();
+  console.log("Appodeal project not found. Click new project button");
+  run_script('angular.element(jQuery("#projects-create")).controller().openCreateProjectDialog()')
 
   project_name_interval = setInterval( wait_for_project_name, 2000 );
 }, 2000);
