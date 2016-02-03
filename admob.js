8b86dd41b78022317c2b64ba49875b4f685fdcd0
Roman Ralovets <roman@ralovets.com>
2016-02-03 11:49:53 +0700

    Move progress bar to content scripts

--- admob.js
+++ admob.js
@@ -33,82 +33,6 @@ var admobStoreIds = [];
 // top progress bar element
 var progressBar;
 
-// progress bar init
-var ProgressBar = function() {
-  if (jQuery("#progress").length == 0) {
-    // top progress bar style
-    var progressBarDiv = '<div id="progress" style="position: fixed; top: 0px; width: 0%; height: 8px; z-index: 10000; left: 0px; background: #6d6d6d;"></div>';
-    // create element
-    jQuery("body").append(progressBarDiv);
-  }
-  this.bar = jQuery("#progress");
-  // current app number and count
-  this.currentAppNum = 0;
-  this.appCount = 0;
-
-  // default ad units num
-  this.defaultAdunitsNum = 6;
-  this.currentDefaultAdunit = 0;
-  this.adunitsNum = INTERSTITIAL_BIDS.length + BANNER_BIDS.length + MREC_BIDS.length + this.defaultAdunitsNum;
-
-  this.bidAdunitsNum = INTERSTITIAL_BIDS.length;
-  this.currentBidAdunit = 0;
-
-  this.bannerAdunitsNum = BANNER_BIDS.length;
-  this.currentBannerAdunit = 0;
-
-  this.mrecAdunitsNum = MREC_BIDS.length;
-  this.currentMrecAdunit = 0;
-
-  console.log("Progress bar added");
-};
-
-// move progress indicator
-ProgressBar.prototype.setPosition = function(position) {
-  this.position = position;
-  var percentage = this.position * 100 + "%";
-  this.bar.css({width: percentage});
-};
-
-// move progress indicator considering processed ad units num
-ProgressBar.prototype.update = function() {
-  var adunitsProcessed = this.currentDefaultAdunit + this.currentBidAdunit + this.currentBannerAdunit + this.currentMrecAdunit;
-  this.position = (this.currentAppNum + adunitsProcessed / this.adunitsNum) / this.appCount;
-  var percentage = this.position * 100 + "%";
-  this.bar.css({width: percentage});
-};
-
-// set progress bar status
-ProgressBar.prototype.setIntegerPosition = function(i) {
-  var position = i / this.appCount;
-  this.setPosition(position);
-  // reset adunits counters
-  this.currentDefaultAdunit = 0;
-  this.currentBidAdunit = 0;
-  this.currentBannerAdunit = 0;
-  this.currentMrecAdunit = 0;
-}
-
-ProgressBar.prototype.increaseDefaultCounter = function() {
-  this.currentDefaultAdunit += 1;
-  this.update();
-}
-
-ProgressBar.prototype.increaseBidCounter = function() {
-  this.currentBidAdunit += 1;
-  this.update();
-}
-
-ProgressBar.prototype.increaseBannerCounter = function() {
-  this.currentBannerAdunit += 1;
-  this.update();
-}
-
-ProgressBar.prototype.increaseMrecCounter = function() {
-  this.currentMrecAdunit += 1;
-  this.update();
-}
-
 chrome.storage.local.get("admob_processing", function(result) {
   if (result['admob_processing']) {
     document.body.onload = function() {
@@ -1388,7 +1312,7 @@ function create_adunit_loop(bid_floors, admob_app, token, complete) {
 
 // send logs to server in case of error with special error mark
 window.onerror = function() {
-  var errorMessage = "admob.js error";
-  console.log(errorMessage);
+  // var errorMessage = "Admob error";
+  // console.log(errorMessage);
   // sendAndFlushLogs(3, 1, errorMessage);
 }
