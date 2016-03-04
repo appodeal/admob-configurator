de72f10a679a194fc2a00c68b6de5ee8a5b1f7de
Roman Ralovets <roman@ralovets.com>
2016-03-05 00:40:54 +0700

    create missing adunits with ability to make finish report

--- libs/admob.js
+++ libs/admob.js
@@ -2,7 +2,16 @@ var Admob = function(userId, apiKey) {
   console.log("Initialize admob" + " (" + userId + ", " + apiKey + ")");
   this.userId = userId;
   this.apiKey = apiKey;
+  // internal admob request url
   Admob.inventoryUrl = "https://apps.admob.com/tlcgwt/inventory";
+  // internal admob params
+  Admob.types = {text: 0, image: 1, video: 2};
+  // appodeal ad unit params
+  Admob.adTypes = {interstitial: 0, banner: 1, video: 2, native: 3, mrec: 4};
+  // adunits bids
+  Admob.interstitialBids = [0.15, 0.25, 0.65, 0.8, 1.25, 2.15, 2.5, 5.0, 7.5, 10.0, 12.5, 15.0];
+  Admob.bannerBids = [0.1, 0.2, 0.35, 0.5, 0.7];
+  Admob.mrecBids = [0.15, 0.3, 0.6, 0.8, 1.25, 2];
 };
 
 // get appodeal apps
@@ -12,7 +21,7 @@ var Admob = function(userId, apiKey) {
 // link Admob apps to play market or itunes
 // send local apps and adunits to appodeal
 Admob.prototype.syncInventory = function(callback) {
-  console.log("Syncing inventory");
+  console.log("Sync inventory");
   var self = this;
   self.getRemoteInventory(function() {
     self.getLocalInventory(function() {
@@ -21,15 +30,16 @@ Admob.prototype.syncInventory = function(callback) {
       self.mapApps();
       self.createMissingApps(function() {
         self.linkApps(function() {
+          self.makeMissingAdunitsList();
           self.createMissingAdunits(function() {
             self.syncWithServer(function() {
               callback();
-            });
-          });
-        });
-      });
-    });
-  });
+            })
+          })
+        })
+      })
+    })
+  })
 }
 
 // make a request to admob inventory url
@@ -81,6 +91,99 @@ Admob.adUnitTypeRegex = function(name) {
   if (matchedType && matchedType.length > 1) {
     return (matchedType[2]);
   }
+}
+
+// make scheme array from existing local adunits to compare it with the full scheme and find missing
+Admob.localAdunitsToScheme = function(app) {
+  var scheme = [];
+  app.localAdunits.forEach(function(adunit) {
+    var adTypeName = Admob.adUnitTypeRegex(adunit[3]);
+    var admobAppId = app.localApp[1];
+    var adType = adunit[14];
+    var formats = adunit[16];
+    var adFormatName;
+    // text format name only for text adunits without bid floors
+    if (!adunit[10] && formats.length == 1 && formats[0] == 0) {
+      adFormatName = "text";
+    } else {
+      adFormatName = "image";
+    }
+    var bid;
+    var name;
+    var hash;
+    if (adunit[10]) {
+      bid = adunit[10][0][5][1][1];
+      var floatBid = parseInt(bid) / 1000000;
+      name = Admob.adunitName(app, adTypeName, adFormatName, floatBid);
+      hash = {app: admobAppId, name: name, adType: adType, formats: formats, bid: bid};
+    } else {
+      name = Admob.adunitName(app, adTypeName, adFormatName)
+      hash = {app: admobAppId, name: name, adType: adType, formats: formats};
+    }
+    scheme.push(hash);
+  })
+  return(scheme);
+}
+
+// Find all missing adunits for app in inventory
+Admob.adunitsScheme = function(app) {
+  var scheme = [];
+  // default adunits
+  scheme.push({app: app.localApp[1], name: Admob.adunitName(app, "interstitial", "image"), adType: 1, formats: [1]});
+  scheme.push({app: app.localApp[1], name: Admob.adunitName(app, "interstitial", "text"), adType: 1, formats: [0]});
+  scheme.push({app: app.localApp[1], name: Admob.adunitName(app, "banner", "image"), adType: 0, formats: [1]});
+  scheme.push({app: app.localApp[1], name: Admob.adunitName(app, "banner", "text"), adType: 0, formats: [0]});
+  scheme.push({app: app.localApp[1], name: Admob.adunitName(app, "mrec", "image"), adType: 0, formats: [1]});
+  scheme.push({app: app.localApp[1], name: Admob.adunitName(app, "mrec", "text"), adType: 0, formats: [0]});
+  // adunit bid floor in admob format
+  function admobBidFloor(bid) {
+    return (bid * 1000000).toString();
+  }
+  // adunits with bid floors
+  // interstitial adunits
+  Admob.interstitialBids.forEach(function(bid) {
+    var name = Admob.adunitName(app, "interstitial", "image", bid);
+    scheme.push({app: app.localApp[1], name: name, adType: 1, formats: [0, 1], bid: admobBidFloor(bid)})
+  })
+  // banner adunits
+  Admob.bannerBids.forEach(function(bid) {
+    var name = Admob.adunitName(app, "banner", "image", bid);
+    scheme.push({app: app.localApp[1], name: name, adType: 0, formats: [0, 1], bid: admobBidFloor(bid)})
+  })
+  // mrec adunits
+  Admob.mrecBids.forEach(function(bid) {
+    var name = Admob.adunitName(app, "mrec", "image", bid);
+    scheme.push({app: app.localApp[1], name: name, adType: 0, formats: [0, 1], bid: admobBidFloor(bid)})
+  })
+  return (scheme);
+}
+
+// Find all missing adunits for app in inventory
+Admob.missingAdunits = function(app) {
+  var scheme = Admob.adunitsScheme(app);
+  var localScheme = Admob.localAdunitsToScheme(app);
+  var missingScheme = [];
+  // select all elements from scheme that are not existing in localScheme
+  var missingScheme = $.grep(scheme, function(s) {
+    return $.grep(localScheme, function(l) {
+      return (JSON.stringify(s) == JSON.stringify(l));
+    }).length == 0
+  })
+  return (missingScheme);
+}
+
+// generate adunit name
+Admob.adunitName = function(app, adName, typeName, bidFloor) {
+  var name = "Appodeal/" + app.id + "/" + adName + "/" + typeName;
+  if (bidFloor) {
+    name += "/" + bidFloor;
+  }
+  // max adunit name length equals 80, allocate the rest of name to bundle id
+  var bundleLength = 80 - name.length - 1;
+  if (bundleLength > 0) {
+    name += "/" + app.bundle_id.substring(0, bundleLength);
+  }
+  return (name);
 }
 
 // get remote appodeal apps with adunits
@@ -203,7 +306,7 @@ Admob.prototype.selectLocalAdunits = function(admobAppId) {
   return (selectedAdunits);
 }
 
-// Create local apps for all apps from inventory with missign local apps
+// Create local apps for all apps from inventory with missing local apps
 Admob.prototype.createMissingApps = function(callback) {
   console.log("Create missing apps");
   var self = this;
@@ -244,10 +347,35 @@ Admob.prototype.linkApps = function(callback) {
   })
 }
 
+// find missing local adunits
+Admob.prototype.makeMissingAdunitsList = function() {
+  console.log("Make missing adunits list");
+  var self = this;
+  self.missingAdunits = [];
+  self.inventory.forEach(function(app, index, apps) {
+    var list = Admob.missingAdunits(app);
+    self.missingAdunits.push.apply(self.missingAdunits, list);
+  })
+}
+
 // create local adunits in local apps
 Admob.prototype.createMissingAdunits = function(callback) {
   console.log("Create missing adunits");
-  callback();
+  var self = this;
+  // create missing local adunits
+  Admob.synchronousEach(self.missingAdunits.slice(0, 5), function(s, next) {
+    self.createLocalAdunit(s, function() {
+      // set newly created local app for remote app in inventory
+      // var inventoryAppIndex = $.inArray(app, self.inventory);
+      // if (inventoryAppIndex > -1) {
+      //   self.inventory[inventoryAppIndex].localApp = localApp;
+      //   next();
+      // }
+      next();
+    })
+  }, function() {
+    callback();
+  })
 }
 
 // send information about local apps and adunits to server
@@ -368,4 +496,57 @@ Admob.prototype.addStoreId = function(storeId) {
   } else {
     self.storeIds = [storeId];
   }
+}
+
+// create local adunit from scheme
+Admob.prototype.createLocalAdunit = function(s, callback) {
+  console.log("Create adunit " + s.name);
+  var self = this;
+  params = {
+    "method": "insertInventory",
+    "params": {
+      "3": {
+        "2": s.app,
+        "3": s.name,
+        "14": s.adType,
+        "16": s.formats
+      }
+    },
+    "xsrf": self.token
+  }
+  Admob.inventoryPost(params, function(data) {
+    var localAdunit = data.result[1][2][0];
+    // insert mediation
+    if (s.bid) {
+      var mediationParams = {
+        "method": "updateMediation",
+        "params": {
+          "2": s.app,
+          "3": localAdunit[1],
+          "4": [
+            {
+              "2": 1,
+              "3": "1",
+              "5": {
+                "1": {
+                  "1": s.bid,
+                  "2": "USD"
+                }
+              },
+              "7": 0,
+              "9": 1
+            }
+          ],
+          "5": 0
+        },
+        "xsrf": self.token
+      }
+      Admob.inventoryPost(mediationParams, function(data) {
+        var localAdunit = data.result[1][2][0];
+        callback(localAdunit);
+      })
+    } else {
+      callback(localAdunit);
+    }
+  })
 }
