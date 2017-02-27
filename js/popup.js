b7076d039e44fe0ebc87687cbdcfaec3bf3e7bd2
kirill.holodyuk <kirill.holodyuk@gmail.com>
2017-02-27 12:54:04 +0200

    interim commit

--- js/popup.js
+++ js/popup.js
@@ -1,41 +1,137 @@
-var button_logout = '<a id="logout_link" class="button_logout right">Logout</a>';
-var LoadController = function () {
-    function return_link(event) {
+var LoadController, button_logout, email, length_email;
+
+length_email = 25;
+
+button_logout = '<a id="logout_link" class="button_logout right">Logout</a>';
+
+email = null;
+
+LoadController = (function () {
+    var addCountApps, addDoneLabel, addEnableReport, addNoApps, clearStorageAndCookies, faq_link, getAppodealStatus, getLocalStatus, getRemoteStatus, load_hover, login_link, logout_link, return_link, setEventListen, storage_local_get, updateAppodealCredentials, login, http, result, localCredentials, reporting_link, admob_link;
+    return_link = function (event) {
         console.log('return_link');
-        chrome.tabs.update({url: APPODEAL_URL_SSL_SIGN});
+        chrome.tabs.update({
+            url: APPODEAL_URL_SSL_SIGN
+        });
         window.close();
-    }
-
-    function faq_link(event) {
+    };
+    faq_link = function (event) {
         console.log('faq_link');
-        chrome.tabs.update({url: FAQ_LINK});
+        chrome.tabs.update({
+            url: FAQ_LINK
+        });
         window.close();
-    }
-
-    function load_hover(event) {
+    };
+    login_link = function (event) {
+        console.log('login_link');
+        chrome.tabs.update({
+            url: APPODEAL_URL_SSL + '/signin'
+        });
+        window.close();
+    };
+    logout_link = function (event) {
+        console.log('logout_link');
+        clearStorageAndCookies();
+        chrome.tabs.update({
+            url: APPODEAL_URL_SSL
+        });
+        window.close();
+    };
+    load_hover = function (event) {
         console.log('load_hover');
-        $('#main .row').hover(
-            function () {
-                if ($(this).find(".gray").length > 0) {
-                    $(this).css("background", "#FACFC8");
-                    $(this).find("a.point").addClass("linkWhite");
-                } else if (!$(this).find(".userActive.svgStep").length > 0) {
-                    $(this).css("background", "#EC3F21");
-                    $(this).find(".backgroundRadius").css("background", "#EC3F21");
-                    $(this).find(".svgStep").addClass("active");
-                    $(this).find("a.point").addClass("linkWhite");
-                }
-            },
-            function () {
-                $(this).css("background", "#ffffff");
-                $(this).find(".backgroundRadius").css("background", "#ffffff");
-                $(this).find(".svgStep").removeClass("active");
-                $(this).find("a.point").removeClass("linkWhite");
+        $('#main .row').hover((function () {
+            if ($(this).find('.gray').length > 0) {
+                $(this).css('background', '#FACFC8');
+                $(this).find('a.point').addClass('linkWhite');
+            } else if (!$(this).find('.userActive.svgStep').length > 0) {
+                $(this).css('background', '#EC3F21');
+                $(this).find('.backgroundRadius').css('background', '#EC3F21');
+                $(this).find('.svgStep').addClass('active');
+                $(this).find('a.point').addClass('linkWhite');
             }
-        );
-    }
-
-    function storage_local_get() {
+        }), function () {
+            $(this).css('background', '#ffffff');
+            $(this).find('.backgroundRadius').css('background', '#ffffff');
+            $(this).find('.svgStep').removeClass('active');
+            $(this).find('a.point').removeClass('linkWhite');
+        });
+    };
+    addDoneLabel = function (btn, text, step, id) {
+        if (btn.length) {
+            btn.html('<i class="ion"><div class="backgroundRadius"></div><div class="' + step + ' svgStep"></div></i><a id="' + id + '" class="point">' + text + '</a>');
+        }
+    };
+    addEnableReport = function (btn) {
+        if (btn.length) {
+            btn.html('<i class="ion"><div class="backgroundRadius"></div><div class="stepTwo svgStep"></div></i><a id="reporting_link" class="point">Enable Admob reporting</a>');
+        }
+    };
+    addCountApps = function (btn, leftNum) {
+        var message;
+        if (btn.length) {
+            message = '';
+            if (leftNum === 1) {
+                message = leftNum + ' app left';
+            }
+            if (leftNum >= 2) {
+                message = leftNum + ' apps left';
+            }
+            btn.html('<i class="ion"><div class="backgroundRadius"></div><div class="stepThree svgStep"></div></i><a id="admob_link" class="point">' + message + '</a>');
+        }
+    };
+    addNoApps = function (btn) {
+        if (btn.length) {
+            btn.html('<i class="ion"><div class="backgroundRadius"></div><div class="stepThree svgStep"></div></i><a id="admob_link" class="point gray">No apps</a>');
+        }
+    };
+    clearStorageAndCookies = function () {
+        chrome.storage.local.clear();
+        chrome.browserAction.setBadgeText({
+            text: ''
+        });
+        chrome.cookies.remove({
+            'url': APPODEAL_URL,
+            'name': '_android_ad_network_session'
+        });
+        chrome.cookies.remove({
+            'url': APPODEAL_URL_SSL,
+            'name': '_android_ad_network_session'
+        });
+        chrome.cookies.remove({
+            'url': APPODEAL_URL,
+            'name': 'remember_token'
+        });
+        chrome.cookies.remove({
+            'url': APPODEAL_URL_SSL,
+            'name': 'remember_token'
+        });
+        chrome.cookies.remove({
+            'url': APPODEAL_URL,
+            'name': 'user_id'
+        });
+        chrome.cookies.remove({
+            'url': APPODEAL_URL_SSL,
+            'name': 'user_id'
+        });
+    };
+    getAppodealStatus = function (complete) {
+        http = new XMLHttpRequest;
+        http.open('GET', APPODEAL_STATUS_URL, true);
+        http.send();
+        http.onreadystatechange = function () {
+            if (http.readyState === 4) {
+                if (http.status === 200) {
+                    result = JSON.parse(http.responseText);
+                    console.log(result);
+                    complete(result);
+                } else if (http.status === 403) {
+                    console.log(http);
+                    clearStorageAndCookies();
+                }
+            }
+        };
+    };
+    storage_local_get = function () {
         console.log('storage_local_get');
         chrome.storage.local.get({
             'appodeal_email': null,
@@ -44,35 +140,136 @@ var LoadController = function () {
             'appodeal_admob_account_id': null
         }, function (items) {
             console.log(items);
-            self.getLocalStatus(items);
-        })
-
-    }
-
-    // get local plugin variables and update menu items
-    function getLocalStatus(items) {
-        var login = $('#login');
-        debugger;
-        if (items['appodeal_email']) {
-            console.log(login);
-            // login.innerHTML = getLogoutText(items, 'userActive', 'logout_link');
-            return '<i class="ion"><div class="backgroundRadius"></div><div class="userActive svgStep"></div></i><a class="not_point">' + items['appodeal_email'] + '</a>' + button_logout;
+            email = cut(items['appodeal_email'], length_email);
+            getLocalStatus(items);
+            getRemoteStatus(items);
+            setEventListen(items);
+        });
+    };
+    updateAppodealCredentials = function (result, callback) {
+        localCredentials = {};
+        if (result['user_id']) {
+            localCredentials['appodeal_user_id'] = result['user_id'];
         } else {
-            // login.innerHTML = getLoginText('stepOne', 'login_link');
+            chrome.storage.local.remove('appodeal_user_id');
         }
-    }
-
-    return {
-        init: function () {
-            $('#return_link').click(return_link);
-            $('#faq_link').click(faq_link);
-            load_hover();
-            storage_local_get();
+        if (result['api_key']) {
+            localCredentials['appodeal_api_key'] = result['api_key'];
+        } else {
+            chrome.storage.local.remove('appodeal_api_key');
+        }
+        if (result['plugin_status']['account']) {
+            localCredentials['appodeal_admob_account_id'] = result['plugin_status']['account'];
+        } else {
+            chrome.storage.local.remove('appodeal_admob_account_id');
+        }
+        if (result['plugin_status']['publisher_id']) {
+            localCredentials['appodeal_admob_account_publisher_id'] = result['plugin_status']['publisher_id'];
+        } else {
+            chrome.storage.local.remove('appodeal_admob_account_publisher_id');
+        }
+        if (result['plugin_status']['email']) {
+            localCredentials['appodeal_admob_account_email'] = result['plugin_status']['email'];
+        } else {
+            chrome.storage.local.remove('appodeal_admob_account_email');
+        }
+        if (result['plugin_status']['adunits']) {
+            localCredentials['adunitsVersion'] = result['plugin_status']['adunits'];
+        } else {
+            chrome.storage.local.remove('adunitsVersion');
+        }
+        if (result['plugin_status']['reporting']) {
+            localCredentials['reportingVersion'] = result['plugin_status']['reporting'];
+        } else {
+            chrome.storage.local.remove('reportingVersion');
+        }
+        if (result['plugin_status']['interstitialBids']) {
+            localCredentials['interstitialBids'] = result['plugin_status']['interstitialBids'];
+        } else {
+            chrome.storage.local.remove('interstitialBids');
+        }
+        if (result['plugin_status']['bannerBids']) {
+            localCredentials['bannerBids'] = result['plugin_status']['bannerBids'];
+        } else {
+            chrome.storage.local.remove('bannerBids');
+        }
+        if (result['plugin_status']['mrecBids']) {
+            localCredentials['mrecBids'] = result['plugin_status']['mrecBids'];
+        } else {
+            chrome.storage.local.remove('mrecBids');
+        }
+        chrome.storage.local.set(localCredentials, function () {
+            callback();
+        });
+    };
+    getLocalStatus = function (items) {
+        console.log('getLocalStatus');
+        login = $('#login');
+        if (items['appodeal_email']) {
+            login.html('<i class="ion"><div class="backgroundRadius"></div><div class="userActive svgStep"></div></i><a class="not_point">' + email + '</a>' + button_logout);
+        } else {
+            login.html('<i class="ion"><div class="backgroundRadius"></div><div class="stepOne svgStep"></div></i><a id="login_link" class="point">Login to Appodeal</a>');
         }
     };
-}();
+    getRemoteStatus = function (items) {
+        console.log('getRemoteStatus');
+        if (items['appodeal_email']) {
+            getAppodealStatus(function (result) {
+                updateAppodealCredentials(result, function () {
+                    var data, leftNum;
+                    data = result['plugin_status'];
+                    console.log(data);
+                    leftNum = data['total'] - data['synced'];
+                    console.log(leftNum);
+                    if (data['account']) {
+                        addDoneLabel($('#reporting'), 'Enabled Admob reporting', 'stepDone', 'reporting_link');
+                        addDoneLabel($('#admob'), 'Sync Appodeal and Admob ad units', 'stepDone', 'admob_link');
+                    } else {
+                        addEnableReport($('#reporting'));
+                        return null;
+                    }
+                    if (leftNum) {
+                        addCountApps($('#admob'), leftNum);
+                    } else if (data['total']) {
+                        addDoneLabel($('#admob'), 'Synced Appodeal and Admob ad units', 'stepDone', 'admob_link');
+                    } else {
+                        addNoApps($('#admob'));
+                    }
+                    $('#reporting_link').click(reporting_link);
+                    $('#admob_link').click(admob_link);
+                });
+            });
+        }
+    };
+    reporting_link = function (event) {
+        chrome.tabs.update({url: "https://apps.admob.com/#home"}, function (tab) {
+            chrome.storage.local.set({"reporting_tab_id": tab.id});
+            window.close();
+        });
+    };
+    admob_link = function (event) {
+        chrome.tabs.update({url: "https://apps.admob.com/#monetize/reporting:admob/d=1&cc=USD"}, function (tab) {
+            chrome.storage.local.set({"admob_processing": true}, function () {
+                window.close();
+            });
+        });
+    };
+    setEventListen = function (items) {
+        $('#login_link').click(login_link);
+        $('#logout_link').click(logout_link);
 
-$(document).ready(function () {
+        $('#return_link').click(return_link);
+        $('#faq_link').click(faq_link);
+    };
+    return {
+        init: function () {
+            load_hover();
+            storage_local_get();
+            setEventListen();
+        }
+    };
+})();
+
+$(document).ready(function(){
     LoadController.init();
-    // sendNotification('FACK','FACK FACK');
 });
--- js/utils.js
+++ js/utils.js
@@ -170,3 +170,14 @@ var sendNotification = function(title, ms) {
         function() {}
     );
 };
+
+function cut(text, limit) {
+    text = text.trim();
+    if (text.length <= limit) return text;
+    text = text.slice(0, limit);
+    var lastSpace = text.lastIndexOf(" ");
+    if (lastSpace > 0) {
+        text = text.substr(0, lastSpace);
+    }
+    return text + "...";
+}
