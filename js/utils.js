bea1076edc829f485611b97e4e1b98057afd5a7e
kirill.holodyuk <kirill.holodyuk@gmail.com>
2017-03-20 19:45:39 +0200

    add many accounts

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,6 @@
 var icon_url = '../img/icon/icon-64.png';
-var APPODEAL_URL = "http://staging.appodeal.com";
-var APPODEAL_URL_SSL = "https://staging.appodeal.com";
+var APPODEAL_URL = "http://www.appodeal.com";
+var APPODEAL_URL_SSL = "https://www.appodeal.com";
 var APPODEAL_URL_SSL_SIGN = APPODEAL_URL_SSL + "/signin";
 var APPODEAL_STATUS_URL = APPODEAL_URL_SSL + "/api/v2/get_api_key";
 var FAQ_LINK = 'https://github.com/appodeal/admob-configurator/wiki/FAQ';
@@ -104,7 +104,7 @@ function sendLogs(apiKey, userId, mode, part, version, items, callback) {
     var params = JSON.stringify(json);
     $.ajax({
         method: "POST",
-        url: "https://staging.appodeal.com/api/v2/save_extension_logs",
+        url: "https://www.appodeal.com/api/v2/save_extension_logs",
         contentType: "application/json",
         dataType: "json",
         data: params
@@ -179,9 +179,5 @@ function cut(text, limit) {
     text = text.trim();
     if (text.length <= limit) return text;
     text = text.slice(0, limit);
-    var lastSpace = text.lastIndexOf(" ");
-    // if (lastSpace > 0) {
-    //     text = text.substr(0, lastSpace);
-    // }
     return text + "...";
 }
