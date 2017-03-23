d2ee5094331f7cb1e44848effb936eced511cf35
kirill.holodyuk <kirill.holodyuk@gmail.com>
2017-03-24 01:44:25 +0200

    bumb 15.2

--- js/admob.js
+++ js/admob.js
@@ -419,7 +419,16 @@ Admob.prototype.getRemoteInventory = function(callback) {
   }
   $.get(Admob.remoteInventoryUrl, json)
     .done(function(data) {
-      self.inventory = data.applications;
+      // self.inventory = data.applications;
+      self.inventory = [];
+      data.applications.forEach(function(item, i, arr) {
+          var os = 4;
+          if(item.os == 4 || item.os == 5){
+              os = 1
+          }
+          item.os = os;
+          self.inventory.push(item);
+      });
       if (self.inventory && self.inventory.length) {
         callback();
       } else {
