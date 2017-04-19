8aaede9e2f425e40d66b220ac6d50f01acb9cb45
kirill.holodyuk <kirill.holodyuk@gmail.com>
2017-04-19 14:10:25 +0300

    interim commit

--- js/library.js
+++ js/library.js
@@ -1,16 +1,29 @@
 sendOut(0, "Create new project from the library page (new accounts)");
 var LibraryController, modal;
+//element DOM
+var p6n_dropdown_menu = 'pan-platform-bar-project-switcher p6n-dropdown-menu';
 
 LibraryController = function () {
     return {
-        find_project: function () {
-            debugger;
+        find: function () {
+            var dropdown_menu = $(p6n_dropdown_menu);
+            if (!dropdown_menu.length){
+                //Create project
+                LibraryController.create();
+            }
+        },
+        create: function () {
+            run_script("angular.element('pan-platform-bar-project-switcher').triggerHandler('click');")
         }
     }
 }();
 
 $(document).ready(function () {
     setTimeout(function () {
-        LibraryController.find_project();
+        appendJQuery(function () {
+            modal = new Modal();
+            modal.show("Appodeal Chrome Extension", "Find Appodeal project. Please wait");
+        });
+        LibraryController.find();
     }, 500);
 });
