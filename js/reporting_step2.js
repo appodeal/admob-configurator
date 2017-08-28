b4fb63eeda1c8d8e863c6f4fe9d750e3725e5d6c
Kirill Holodyuk <kirill.holodyuk@gmail.com>
2017-08-28 08:19:34 +0300

    code refactoring

--- js/reporting_step2.js
+++ js/reporting_step2.js
@@ -1,44 +1,53 @@
-sendOut(0, "Open project configuration.");
-var modal, adsence_enabling_interval = null, airbrake;
+var ReportingStepTwoController, modal, adsence_enabling_interval, airbrake;
 
-function projectToLocation() {
-    document.location.href = projectConsentUrl(locationProjectName());
-}
-function wait_for_adsence_btn() {
-    var enableApiBtn, disableBtnCode, disableApiBtn, enableApiBtnOld, disableBtnCodeOld, disableApiBtnOld, code;
-    try {
-        enableApiBtn = $("pan-action-bar-button[icon='start'][action='apiCtrl.toggleApi()']:not(.ng-hide) button");
-        disableBtnCode = "pan-action-bar-button[icon='stop'][action='apiCtrl.toggleApi()']:not(.ng-hide) button";
-        disableApiBtn = $(disableBtnCode);
+ReportingStepTwoController = (function() {
+    var wait_for_adsence_btn, projectToLocation, initOtherLibrary;
+    wait_for_adsence_btn = function() {
+        var enableApiBtn, disableBtnCode, disableApiBtn, enableApiBtnOld, disableBtnCodeOld, disableApiBtnOld, code;
+        try {
+            enableApiBtn = $("pan-action-bar-button[icon='start'][action='apiCtrl.toggleApi()']:not(.ng-hide) button");
+            disableBtnCode = "pan-action-bar-button[icon='stop'][action='apiCtrl.toggleApi()']:not(.ng-hide) button";
+            disableApiBtn = $(disableBtnCode);
 
-        enableApiBtnOld = $("[ng-if='!apiCtrl.isServiceEnabled()']");
-        disableBtnCodeOld = "[ng-if='apiCtrl.isServiceEnabled()']";
-        disableApiBtnOld = $(disableBtnCodeOld);
-        if ((enableApiBtn.length || enableApiBtnOld.length)) {
-            clearInterval(adsence_enabling_interval);
-            if (enableApiBtn.length) {
-                enableApiBtn.click();
-            } else {
-                code = "angular.element(jQuery(\"[ng-if='!apiCtrl.isServiceEnabled()']\")).controller().toggleApi();";
-                run_script(code);
-            }
-            waitForElement(disableBtnCode + ", " + disableBtnCodeOld, null, function (element) {
+            enableApiBtnOld = $("[ng-if='!apiCtrl.isServiceEnabled()']");
+            disableBtnCodeOld = "[ng-if='apiCtrl.isServiceEnabled()']";
+            disableApiBtnOld = $(disableBtnCodeOld);
+            if ((enableApiBtn.length || enableApiBtnOld.length)) {
+                clearInterval(adsence_enabling_interval);
+                if (enableApiBtn.length) {
+                    enableApiBtn.click();
+                } else {
+                    code = "angular.element(jQuery(\"[ng-if='!apiCtrl.isServiceEnabled()']\")).controller().toggleApi();";
+                    run_script(code);
+                }
+                waitForElement(disableBtnCode + ", " + disableBtnCodeOld, null, function (element) {
+                    projectToLocation()
+                })
+            } else if ((disableApiBtn.length || disableApiBtnOld.length)) {
                 projectToLocation()
-            })
-        } else if ((disableApiBtn.length || disableApiBtnOld.length)) {
-            projectToLocation()
+            }
+        } catch (err) {
+            airbrake.setError(err);
+            throw err;
         }
-    } catch (err) {
-        airbrake.setError(err);
-        throw err;
-    }
-}
-
-jQuery(function () {
-    appendJQuery(function () {
+    };
+    projectToLocation = function () {
+        document.location.href = projectConsentUrl(locationProjectName());
+    };
+    initOtherLibrary = function () {
+        sendOut(0, "Open project configuration.");
         airbrake = new AirbrakeController();
         modal = new Modal();
         modal.show("Appodeal Chrome Extension", "Enabling the AdSense Management API");
-        adsence_enabling_interval = setInterval(wait_for_adsence_btn, 2000);
-    });
+    };
+    return {
+        init: function() {
+            initOtherLibrary();
+            adsence_enabling_interval = setInterval(wait_for_adsence_btn, 2000);
+        }
+    };
+})();
+
+$(document).ready(function() {
+    ReportingStepTwoController.init();
 });
--- js/reporting_step3.js
+++ js/reporting_step3.js
@@ -1,67 +1,75 @@
-sendOut(0, 'Set project name and save');
-var modal, airbrake, consents_interval = null, email_credentials = null;
-function getEmail() {
-    Utils.injectScript('\
+var ReportingStepThreeController, modal, airbrake, consents_interval, email_credentials;
+
+ReportingStepThreeController = (function() {
+    var initOtherLibrary, wait_for_consents, setEmailCredential, getEmail;
+    wait_for_consents = function () {
+        var save_button, script, console_log_code, select_save_code, name_code, set_val_code, code;
+        try {
+            save_button = jQuery("jfk-button[jfk-on-action='ctrl.submit()']");
+            if (save_button.length) {
+                clearInterval(consents_interval);
+                appendJQuery(function () {
+                    script = document.createElement('script');
+                    console_log_code = "console.log('Set project name and save'); ";
+                    select_save_code = "jQuery(\"jfk-button[jfk-on-action='ctrl.submit()']\")";
+                    name_code = "jQuery(\"[ng-model='ctrl.data.displayName']\")";
+                    set_val_code = name_code + ".val('Appodeal Revenue');" + "angular.element(" + name_code + ").triggerHandler('input');";
+                    code = console_log_code + set_val_code + "setTimeout(function() {angular.element(" + select_save_code + ").controller().submit();}, 1000);";
+                    script.appendChild(document.createTextNode(code));
+                    document.getElementsByTagName('head')[0].appendChild(script);
+                    console.log("Save button clicked");
+                    getEmail();
+                    setEmailCredential(locationProjectName());
+                });
+            }
+        } catch (err) {
+            airbrake.setError(err);
+            throw err;
+        }
+    };
+    initOtherLibrary = function (massage) {
+        sendOut(0, massage);
+        airbrake = new AirbrakeController();
+        modal = new Modal();
+        modal.show("Appodeal Chrome Extension", massage);
+    };
+    setEmailCredential = function (project_name) {
+        chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+            if (request.type === "to_console") {
+                try {
+                    email_credentials = request.data.pantheon_account_chooser_data[1][4];
+                } catch (err) {
+                    email_credentials = null
+                }
+                chrome.storage.local.set({
+                    "email_credentials": email_credentials
+                });
+                window.setInterval(function () {
+                    if (project_name.length) {
+                        console.log("Find name", project_name);
+                        document.location.href = credentialPageUrl(project_name);
+                    }
+                }, 5000);
+            }
+        });
+    };
+    getEmail = function () {
+        Utils.injectScript('\
         chrome.runtime.sendMessage("' + chrome.runtime.id + '", {type: "console_email_notification", pantheon_account_chooser_data: pantheon_account_chooser_data },\
         function(response) {\
             if (!response.success)\
                 handleError("console_email_notification");\
         });\
       ');
-}
-
-function setEmailCredential(project_name) {
-    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-        if (request.type === "to_console") {
-            try {
-                email_credentials = request.data.pantheon_account_chooser_data[1][4];
-            } catch (err) {
-                email_credentials = null
-            }
-            chrome.storage.local.set({
-                "email_credentials": email_credentials
-            });
-            window.setInterval(function () {
-                if (project_name.length) {
-                    console.log("Find name", project_name);
-                    document.location.href = credentialPageUrl(project_name);
-                }
-            }, 5000);
+    };
+    return {
+        init: function() {
+            initOtherLibrary('Saving the consent screen.');
+            consents_interval = setTimeout(wait_for_consents, 5000);
         }
-    });
-}
+    };
+})();
 
-function wait_for_consents() {
-    var save_button, script, console_log_code, select_save_code, name_code, set_val_code, code;
-    try {
-        save_button = jQuery("jfk-button[jfk-on-action='ctrl.submit()']");
-        if (save_button.length) {
-            clearInterval(consents_interval);
-            appendJQuery(function () {
-                script = document.createElement('script');
-                console_log_code = "console.log('Set project name and save'); ";
-                select_save_code = "jQuery(\"jfk-button[jfk-on-action='ctrl.submit()']\")";
-                name_code = "jQuery(\"[ng-model='ctrl.data.displayName']\")";
-                set_val_code = name_code + ".val('Appodeal Revenue');" + "angular.element(" + name_code + ").triggerHandler('input');";
-                code = console_log_code + set_val_code + "setTimeout(function() {angular.element(" + select_save_code + ").controller().submit();}, 1000);";
-                script.appendChild(document.createTextNode(code));
-                document.getElementsByTagName('head')[0].appendChild(script);
-                console.log("Save button clicked");
-                getEmail();
-                setEmailCredential(locationProjectName());
-            });
-        }
-    } catch (err) {
-        airbrake.setError(err);
-        throw err;
-    }
-}
-
-jQuery(function () {
-    appendJQuery(function () {
-        modal = new Modal();
-        modal.show("Appodeal Chrome Extension", "Saving the consent screen.");
-        airbrake = new AirbrakeController();
-        consents_interval = setTimeout(wait_for_consents, 5000);
-    });
+$(document).ready(function() {
+    ReportingStepThreeController.init();
 });
