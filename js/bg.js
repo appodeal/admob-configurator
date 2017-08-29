23737298e64d691bd1da7a5dcc5b600a2c9b759c
Kirill Holodyuk <kirill.holodyuk@gmail.com>
2017-08-29 10:01:04 +0300

    code review

--- js/bg.js
+++ js/bg.js
@@ -95,12 +95,8 @@ BackgroundController = (function () {
     webNavigation = function () {
         try {
             chrome.webNavigation.onCompleted.addListener(function (details) {
-                console.log("Current tabId: " + details.tabId);
                 chrome.storage.local.get("reporting_tab_id", function (result) {
-                    console.log("Working Tab ID: ", result['reporting_tab_id'], " Url: ", details.url.toString());
-                    console.log("Url match: ", details.url.toString().match(/project\/([^\/]+)\/?$/));
                     if (result['reporting_tab_id'] && details.tabId.toString() == result['reporting_tab_id'].toString()) {
-                        console.log('second matched');
                         var details_url = details.url.toString();
                         if (details_url.match(/\/apiui\/credential/) || details_url.match(/credentials\?project=/) || details_url.match(/credentials\/oauthclient\?project=/) || details_url.match(/credentials\?highlightClient=/) || details_url.match(/apis\/credentials\/oauthclient\//)) {
                             console.log("calling reporting_step4.js");
