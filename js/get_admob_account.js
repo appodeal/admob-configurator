c892ed8b58bb1bc4c26ee8bbf5e53093b8da209d
Kirill Holodyuk <kirill.holodyuk@gmail.com>
2018-01-09 13:52:22 +0300

    bump 15.12.13

--- js/get_admob_account.js
+++ js/get_admob_account.js
@@ -1,13 +1,19 @@
-var AdmobAccountController;
+var AdmobAccountController, Url;
 
 AdmobAccountController = (function () {
-    var initOtherLibrary, latestCriticalReportingApi, modal, criticalVersion, currentVersion, admob_account_id, message;
+    var initOtherLibrary, latestCriticalReportingApi, modal, criticalVersion, currentVersion, admob_account_id, message, notLogged;
     initOtherLibrary = function (message) {
         sendOut(0, message);
         appendJQuery(function () {
             modal = new Modal();
             modal.show("Appodeal Chrome Extension", message);
         });
+    };
+    notLogged = function () {
+        sendOut(0, "Can't proceed to enabling AdSense Reporting API (not logged in?)");
+        message = "Can't proceed to enabling AdSense Reporting API. If you are not logged in, please authorize and try again.";
+        modal.show('Appodeal Chrome Extension', message);
+        chrome.storage.local.remove('reporting_tab_id');
     };
     latestCriticalReportingApi = function () {
         criticalUpdates(function (updates) {
@@ -28,10 +34,7 @@ AdmobAccountController = (function () {
                                 console.log('Done! redirecting back.');
                                 document.location.href = GOOGLE_CLOUD_CONSOLE_CREDENTIAL;
                             } else {
-                                sendOut(0, "Can't proceed to enabling AdSense Reporting API (not logged in?)");
-                                message = "Can't proceed to enabling AdSense Reporting API. If you are not logged in, please authorize and try again.";
-                                modal.show('Appodeal Chrome Extension', message);
-                                chrome.storage.local.remove('reporting_tab_id');
+                                notLogged();
                             }
                         } else {
                             message = "You're using an old version (" + currentVersion + ") of Appodeal Chrome Extension. Please update extensions at <b>chrome://extensions/</b> and try again.";
@@ -47,8 +50,11 @@ AdmobAccountController = (function () {
     };
     return {
         init: function () {
-            initOtherLibrary('Start configure admob reporting api');
-            airbrake.error.call(latestCriticalReportingApi);
+            Url = window.location.href;
+            if (!Url.match(/apps\.admob\.com\/signup\/create-account/)){
+                initOtherLibrary('Start configure admob reporting api');
+                airbrake.error.call(latestCriticalReportingApi);
+            }
         }
     };
 })();
